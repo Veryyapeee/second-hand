6def3c89035736cbcb36c477308898a2e738d718
Cyprian Zamiara <veryyapeee@gmail.com>
2021-04-29 12:24:57 +0200

    build: setup routing,axios,toastify,folder structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,21 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+import AdminRoute from "Routes/AdminRoute";
+
+const Hello = () => {
+  return <span>Protected</span>;
+};
+
 function App() {
-  return <span>Hello world</span>;
+  return (
+    <Switch>
+      <Route exact path="/" render={() => <span>Hello world</span>} />
+      <Route
+        path="/(.+)"
+        render={() => <AdminRoute path="/protected" component={Hello} />}
+      />
+    </Switch>
+  );
 }
 
 export default App;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
+
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { ToastContainer } from "react-toastify";
+
 import App from "./App";
 
+import "react-toastify/dist/ReactToastify.min.css";
+
+const queryClient = new QueryClient();
+export const history = createBrowserHistory();
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <Router history={history}>
+      <React.StrictMode>
+        <ToastContainer />
+        <App />
+        <ReactQueryDevtools initialIsOpen={true} />
+      </React.StrictMode>
+    </Router>
+  </QueryClientProvider>,
   document.getElementById("root")
 );
