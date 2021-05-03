44b91af591260ce0cd8d5e94594ea2e7b7371289
Cyprian Zamiara <veryyapeee@gmail.com>
2021-05-03 13:44:22 +0200

    feature: working on responsive navBar

--- client/src/Organism/HeaderClient/HeaderClient.tsx
+++ client/src/Organism/HeaderClient/HeaderClient.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import NavElement from "Atoms/NavElement/NavElement";
 import HeaderButton from "Atoms/HeaderButton/HeaderButton";
@@ -6,23 +6,63 @@ import HeaderButton from "Atoms/HeaderButton/HeaderButton";
 import { Town } from "Utils/types";
 
 import styles from "./HeaderClient.module.scss";
+import hamburger from "Assets/menu.svg";
 
 interface Props {
   towns: Town[];
 }
 
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
+}
+
 // @props
 // towns - towns objects fetched from API
-
 const HeaderClient: React.FC<Props> = ({ towns }) => {
+  const [sideBar, setSideBar] = useState<boolean>(true);
+  const [windowWidth, setWindowWidth] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowWidth(getWindowDimensions());
+    }
+
+    window.addEventListener("resize", handleResize);
+    console.log(windowWidth);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [windowWidth]);
+
+  const headerClass = sideBar
+    ? [styles.header, styles.resHeader].join("")
+    : styles.header;
   return (
-    <header className={styles.header}>
-      {towns.map((town: Town) => (
-        <NavElement path={`/home/town/${town._id}`} key={town._id}>
-          {town.name}
-        </NavElement>
-      ))}
-      <HeaderButton path="/work">Pracuj u nas!</HeaderButton>
+    <header className={headerClass}>
+      <div className={styles.logo}>
+        Logo{" "}
+        <img
+          src={hamburger}
+          alt="hamburger"
+          className={styles.hamburger}
+          onClick={() => setSideBar(!sideBar)}
+        />
+      </div>
+      <div
+        className={styles.navElements}
+        style={{
+          display: !sideBar && windowWidth.width <= 900 ? "none" : "flex",
+        }}
+      >
+        {towns.map((town: Town) => (
+          <NavElement path={`/home/town/${town._id}`} key={town._id}>
+            {town.name}
+          </NavElement>
+        ))}
+        <HeaderButton path="/work">Pracuj u nas!</HeaderButton>
+      </div>
     </header>
   );
 };
